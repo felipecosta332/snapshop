2472cca6ea722c9fab31da7324e66b3c34cbd268
Felipe Costa <felipecosta776@outlook.com>
2023-08-08 23:59:24 -0300

    filter properties

--- src/reducers/filterReducers.js
+++ src/reducers/filterReducers.js
@@ -1,25 +1,25 @@
 export const filterReducer = (state, action) => {
-    const {type, payload} = action;
-    switch(type){
-        case "PRODUCT_LIST":
-            return {productList: payload.products};
+  const { type, payload } = action;
+  switch (type) {
+    case "PRODUCT_LIST":
+      return { productList: payload.products };
 
-        case "SORT_BY":
-            return;
+    case "SORT_BY":
+      return {...state, sortBy: payload.sortBy};
 
-        case "RATINGS":
-            return;
+    case "RATINGS":
+      return {...state, ratings: payload.ratings};
 
-        case "BEST_SELLER_ONLY":
-            return;
+    case "BEST_SELLER_ONLY":
+      return { ...state, bestSellerOnly: payload.bestSellerOnly };
 
-        case "ONLY_IN_STOCK":
-            return;
+    case "ONLY_IN_STOCK":
+      return {...state, onlyInStock: payload.onlyInStock };
 
-        case "CLEAR_FILTER":
-            return;
-        
-        default:
-            throw new Error("No Case Found!");
-    }
-}
+    case "CLEAR_FILTER":
+      return;
+
+    default:
+      throw new Error("No Case Found!");
+  }
+};
